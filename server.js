3d87d4f4d98749c225cef4fc3f420178120218b0
evalton <evalton.nunes@gmail.com>
2022-07-10 05:38:34 -0300

    configuração server.js express angular

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 4200;
 
-app.use(express.static(__dirname+'/dist/sistema-gestao-entrega-front'));
+app.use(express.static(__dirname+'/dist/sistema_gestao_entrega_front'));
 
 app.get('/*', (req,res)=>{
-    res.sendFile(__dirname+'/dist/sistema-gestao-entrega-front/index.html')
+    res.sendFile(__dirname+'/dist/sistema_gestao_entrega_front/index.html')
 });
 
 app.listen(PORT, () => {
